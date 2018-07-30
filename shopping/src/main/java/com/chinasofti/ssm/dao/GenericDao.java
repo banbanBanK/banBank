54310d0981aa2b453c08fde19f049b565bc599d4
zybnm <zygle123@gmail.com>
2018-07-30 15:24:54 +0800

    delete all

--- shopping/src/main/java/com/chinasofti/ssm/dao/GenericDao.java
+++
@@ -1,9 +0,0 @@
-package com.chinasofti.ssm.dao;
-
-import java.io.Serializable;
-import java.util.List;
-
-public interface GenericDao<Entity extends Serializable, ID extends Object> {
-	Entity findById(ID Id);
-	List<Entity> findAll();
-}
